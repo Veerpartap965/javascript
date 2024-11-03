baa10a5feb6d67322d6d5f94f108702a19e23b2d
Veerpartap965 <veerpartapsingh656@gmail.com>
2024-11-03 14:36:09 +0000

    operations

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,85 @@
+// let score="33abc";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// // coversion of string to number
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// // NaN ==> not a number    it's means
+// let veer=null;
+// let valueInveer=Number(veer)
+// console.log(typeof(veer));
+// console.log(valueInveer);
+// //0 get
+
+// let one=undefined;
+// let valueInone=Number(one)
+// console.log(typeof(one));
+// console.log(valueInone);
+// //NaN
+// let two=true;
+// let valueIntwo=Number(two)
+// console.log(typeof(two));
+// console.log(valueIntwo);
+// //1
+// let he="veerpartap";
+// let valueInhe=Number(he)
+// console.log(typeof(he));
+// console.log(valueInhe);
+
+// //conversion of number to boolean
+// let IsLoggedIn=1
+// let booleanIsLoggedIn=Boolean(IsLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// //1==>true ;0==>false
+// // " "=>false
+// // "veer"==> true
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+   
+
+// //  ******** operations*********
+let value=3
+let negValue=-value
+console.log(negValue);
+
+console.log(2-2);
+console.log(2+2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);   
+
+let str1="Good"
+let str2="Night"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 +"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((2+3)*4%5);
+
+// console.log(+true);
+// console.log(""+);
+let num1,num2,num3;
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++ // prefix ++gamecounter means first increment after initial value or postfix vice versa
+console.log(gameCounter);
+
+
+
+
+
+
+
+
