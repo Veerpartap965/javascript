45aed419b83f615800905faee3b4587140c35526
Veerpartap965 <veerpartapsingh656@gmail.com>
2024-11-03 10:26:51 +0000

    code done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1465676
+let accountEmail="veer@gmail.com"
+var accountCity="jaipur"
+let accountState;
+// accountId=2// not allowed
+
+ 
+accountEmail="veerpartap@gmail.com"
+accountPassword="348547548"
+accountCity="panjab"
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail, accountPassword,accountPassword,accountState])
