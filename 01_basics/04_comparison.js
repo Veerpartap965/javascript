e3563ee3aba21e7a4fac0dfef5d379e41c74f250
Veerpartap965 <veerpartapsingh656@gmail.com>
2024-11-03 14:54:16 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(21);
+console.log(2=!1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0); //this is true because comparison <,>,<=,>=   and equalty ==   work differently
+//  convert null into a number ,they treat null as 0 .
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//   === it check the datatypes
+
