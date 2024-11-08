fc5a39202d0005fffbc866e621385c74fb02bbe4
Veerpartap965 <veerpartapsingh656@gmail.com>
2024-11-08 06:06:21 +0000

    strings2

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,33 +1,64 @@
-const name="veerpartap"
-const repoCount=7
-console.log(name+repoCount+"value");
+// // console.log(23);
+// const name ="veer"
+// const age=19
+// const city ="panjab"
+// const emailId="veer23@gmail.com"
+// const password=23456445
+// const village="sham84"
+// console.table([name,age,city,emailId,password,village]);
+// //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// "use strict";
+// console.log(typeof age);
+// // +++++++++++++++++++++++++++++++++++======
+// const string="beta"
+// let variable= Number(string)
+// console.log(typeof(string));
+// console.log(variable);
 
-console.log(`my name is ${name} and my repoCount is${repoCount}`);
+// const fgd=null
+// console.log(typeof fgd);
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// // +++++++++++++++++++++++++++++
+// const veer=[2,3]
+// let river={
+//     name:"veer",
+//     age:43,
+// }
+// const mom=function()
+// {
+//     console.log("wow");
+// }
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-const gameName=new String('veerpartap')
-console.log(gameName[0]);
-console.log(gameName.__proto__);  // gave objects eg{}
-console.log(gameName.length);
-console.log(gameName.charAt(4));
-console.log(gameName.indexOf('e'));
-console.log(gameName.toUpperCase());
- const stringNew=gameName.substring(1,3);
-//  const stringNew=gameName.substring(-6,3); // -6 nal koi change ne means no reverse
-console.log(stringNew);
-const anotherName1= gameName.slice(0,4) 
-const anotherName= gameName.slice(-8,4) 
-const anotherNameone="     veer    " 
-console.log(anotherNameone);
-console.log(anotherNameone.trim());
+// const vr="veeeerrrrr"
+// console.log(vr.big());
+// let text = "ha";
 
-const url="https://veerpartap.com/veer%20singh"
-console.log(url.replace('%20','_'));
+// console.log(text.<font color="color">);
+const veer ="fgf"
+const age=22
+console.log(`my test is ${veer} and my age is ${age}`);
+const game= new String("veer")
+console.log(game.at(3));
+console.log(game.toUpperCase);
+console.log(game.toLowerCase);
+console.log(game.replace('r'),(_));
+console.log(game.length());
+console.log(game.charAt(5));
+// console.log(game.concat);
+console.log(game.slice(0,5));
+console.log(game.substring(0,5));
+console.log(game[4]);
+console.log(game.split(_));
+const ve="                       v      e   e  r"
+console.log(ve.trim);
 
-console.log(url.includes('veerpartap'));   /// includes use for koi chij aa jo asi pushye aa
 
-console.log(url.includes('wow')); 
 
-console.log(gameName.split('_'));
+
+
 
 
 
