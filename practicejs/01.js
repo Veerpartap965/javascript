b0514d8b3902a690c0d806b9a3c26a8487cb5b23
Veerpartap965 <veerpartapsingh656@gmail.com>
2025-02-27 14:23:58 +0000

    basics

---
+++ practicejs/01.js
@@ -0,0 +1,11 @@
+let age = 19;             // Number
+let name = "Veer";        // String
+let city = "someCity";    // String (instead of boolean true)
+let account;              // Undefined (implicitly)
+let wow = null;           // Null
+
+console.log(typeof age);       // "number"
+console.log(typeof name);      // "string"
+console.log(typeof city);      // "string"
+console.log(typeof account);   // "undefined"
+console.log(typeof wow);       // "object" (null is considered an object in JavaScript)
